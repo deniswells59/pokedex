baaf3ce40e692f026c4c854f7a8e444d84597171
Denis Wells <deniswells59@gmail.com>
2016-04-30 09:16:35 -0700

    cache

--- public/js/services.js
+++ public/js/services.js
@@ -5,7 +5,7 @@ var app = angular.module('pokeApp');
 app.service('Poke', function($http) {
 
   this.getPage = num => {
-    return $http.get(`https://pokeapi.co/api/v2/pokemon/?limit=100&offset=${num}`, { cache: true});
+    return $http.get(`//pokeapi.co/api/v2/pokemon/?limit=100&offset=${num}`, { cache: true});
   }
 
   this.getPokemon = url => {
@@ -13,7 +13,7 @@ app.service('Poke', function($http) {
   }
 
   this.getPokeById = id => {
-    return $http.get(`https://pokeapi.co/api/v2/pokemon/${id}/`, { cache: true});
+    return $http.get(`//pokeapi.co/api/v2/pokemon/${id}/`, { cache: true});
   }
 
   this.getDesc = url => {
